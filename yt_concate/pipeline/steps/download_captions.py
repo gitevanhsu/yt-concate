317ab2235a8c4ab030e29cbd18a32223b2bf61f1
gitevanhsu <aqwe78965@gmail.com>
2021-11-07 22:51:58 +0800

    Successfully read captions into data structure and fixed Step Class. download_captions.py has a problem unsolved.

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 from yt_concate.pipeline.steps.step import Step
@@ -6,21 +5,26 @@ from yt_concate.pipeline.steps.step import StepException
 
 from pytube import YouTube
 
+
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
 
         for url in data:
+            if utils.caption_file_exists(url):
+                print('found existing caption file')
+                continue
+
             try:
                 source = YouTube(url)
-                caption = source.captions.get_by_language_code('a.en')
+                caption = source.captions.get_by_language_code('en')
                 en_caption_convert_to_srt = caption.generate_srt_captions()
                 print(en_caption_convert_to_srt)
 
                 text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
                 text_file.write(en_caption_convert_to_srt)
                 text_file.close()
-            except (KeyError, AttributeError):
+            except (KeyError, AttributeError, ):
                 print("an Error for :", url)
                 continue
 
