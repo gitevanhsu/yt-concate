d7cb504e716196da06d08da452cd13a5fcb61980
gitevanhsu <aqwe78965@gmail.com>
2021-11-17 18:42:44 +0800

    Try Using multithreading to download captions.

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -10,30 +10,15 @@ from pytube import YouTube
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
+        threads = []
         start = time.time()
 
-        def multithread(data, inputs,utils):
-            try:
-                for yt in data:
-                    if utils.caption_file_exists(yt):
-                        print('found existing caption file')
-                        continue
-                    for caption in YouTube(yt.url).caption_tracks:
-                        if caption.name == 'English (auto-generated)':
-                            print('download caption for', yt.id)
-                            caption = caption.xml_caption_to_srt(caption.xml_captions)
-                            text_file = open(yt.get_caption_filepath(), "w", encoding='utf-8')
-                            text_file.write(caption)
-                            text_file.close()
-            except:
-                print('Error from pytube')
-        threads =[]
-        for _ in range(os.cpu_count()):
-            threads.append(Thread(target=multithread(data, inputs,utils)))
+        for i in range(4):
+            threads.append(Thread(target=self.download_cap, args=(data[i::4], utils)))
+        print(threads)
 
         for thread in threads:
             thread.start()
-
         for thread in threads:
             thread.join()
 
@@ -41,3 +26,22 @@ class DownloadCaptions(Step):
         print('took', end - start, 'seconds')
 
         return data
+
+    def download_cap(self, data, utils):
+        try:
+            for yt in data:
+                if utils.caption_file_exists(yt):
+                    print('found existing caption file')
+                    continue
+                for caption in YouTube(yt.url).caption_tracks:
+                    if caption.name == 'English (auto-generated)':
+                        print('download caption for', yt.id)
+                        caption = caption.xml_caption_to_srt(caption.xml_captions)
+                        text_file = open(yt.get_caption_filepath(), "w", encoding='utf-8')
+                        text_file.write(caption)
+                        text_file.close()
+        except:
+            print('Error from pytube')
+
+
+
